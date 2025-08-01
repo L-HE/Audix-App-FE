b2cd4570ce5a9d951d7dc5e2d40b83e92ecd14f3
Kimgaegeol <flying0305@naver.com>
2025-08-01 18:21:23 +0900

    test commit

---
+++ shared/websocket/client.ts
@@ -0,0 +1,36 @@
+import { io, Socket } from 'socket.io-client';
+
+class WebSocketClient {
+    private socket: Socket | null = null;
+    private onAlertCallback: ((data: any) => void) | null = null;
+
+    connect() {
+        console.log('🔌 WebSocket 연결 시도: http://165.246.116.100:3000');
+        this.socket = io('http://165.246.116.100:3000');
+
+        this.socket.on('connect', () => {
+            console.log('✅ WebSocket 연결 성공:', this.socket?.id);
+        });
+
+        this.socket.on('connect_error', (error) => {
+            console.error('❌ WebSocket 연결 실패:', error);
+        });
+
+        this.socket.on('device-alert', (data) => {
+            console.log('🚨 device-alert 이벤트 수신:', data);
+            if (this.onAlertCallback) {
+                this.onAlertCallback(data);
+            }
+        });
+    }
+
+    setOnAlert(callback: (data: any) => void) {
+        this.onAlertCallback = callback;
+    }
+
+    disconnect() {
+        this.socket?.disconnect();
+    }
+}
+
+export const webSocketClient = new WebSocketClient();
